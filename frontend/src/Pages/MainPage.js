57dd36852843aa27d65a51e07173a8e69da18884
Christos Sampakidis <christossabakidi@gmail.com>
2024-03-01 17:00:59 +0200

    initial commit

--- frontend/src/Pages/MainPage.js
+++ frontend/src/Pages/MainPage.js
@@ -10,20 +10,44 @@ const MainPage = () => {
         }
     }, []);
 
+    const addtoService = async (e) => {
+        e.preventDefault();
+        const file = document.getElementById('file').files[0];
+        const formData = new FormData();
+        formData.append('file', file);
+        const response = await fetch('http://file-storage-service:3010/upload', {
+            method: 'POST',
+            headers: {
+                'Authorization': sessionStorage.getItem('token')
+            },
+            body: formData
+        }).then(response => response.json()).then(data => {
+            if (data.status === 200) {
+                return fetch('http://upload_video:3008/save-video', {
+                    method: 'POST',
+                    headers: {
+                        'Authorization': sessionStorage.getItem('token'),
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ name: file.name, path: data.message })
+                })
+                    .then(response => response.json());
+            } else {
+                throw new Error('Error uploading file');
+            }
+        })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }
+
     return (
         <div id="loginform">
             <div>
-                <form>
-                    <h1>Video Streaming</h1>
-                    <div>
-                        <button onClick={() => navigate('/VideoPage')}>Watch Video</button>
-                    </div>
-                    <div>
-                        <button onClick={() => {
-                            sessionStorage.removeItem('token');
-                            navigate('/');
-                        }}>Logout</button>
-                    </div>
+                <form onSubmit={addtoService}>
+                    <label>File</label>
+                    <input type="file" name="file" id="file" />
+                    <button type="submit" value="Upload" > Upload </button>
                 </form>
             </div>
         </div>
